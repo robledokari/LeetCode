551ffe205144817c4b468c04b96dca30b5b18524
Karina Robledo Hernández <robledokari@gmail.com>
2019-05-22 22:27:09 -0500

    Create 947.java

---
+++ Java/947.java
@@ -0,0 +1,17 @@
+/*
+ * robledokari
+ * Leetcode #947
+ */
+
+class Solution{
+  class DSU{
+    private int[] size;
+    private int[] root;
+    private int count:
+    
+    public DSU(int N){
+      size 
+    }
+  }
+
+}
